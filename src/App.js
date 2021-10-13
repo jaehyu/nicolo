5f4d9c2cba9f267778d13954652b0c78614473ca
jaehyu <dlwoguss@gmail.com>
2021-10-13 21:33:44 +0900

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,70 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import styled, { ThemeProvider } from 'styled-components';
+
+import Button from './Components/Button';
+
+const AppBlock = styled.div`
+    width: 512px;
+    margin: 0 auto;
+    margin-top: 4rem;
+    border: 1px solid black;
+    padding: 1rem;
+`;
+
+const ButtonGroup = styled.div`
+    & + & {
+        margin-top: 1rem;
+    }
+`;
+
+const palette = {
+    blue: '#228be6',
+    gray: '#496057',
+    pink: '#f06595',
+};
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    return (
+        <ThemeProvider
+            theme={{
+                palette,
+            }}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+            <AppBlock>
+                <ButtonGroup>
+                    <Button size="large" color="pink">
+                        Button
+                    </Button>
+                    <Button>Button</Button>
+                    <Button size="small" color="gray">
+                        Button
+                    </Button>
+                </ButtonGroup>
+
+                <ButtonGroup>
+                    <Button color="pink" size="large" outline>
+                        Button
+                    </Button>
+                    <Button outline>Button</Button>
+                    <Button size="small" color="gray" outline>
+                        Button
+                    </Button>
+                </ButtonGroup>
+
+                <ButtonGroup>
+                    <Button size="large" fullWidth>
+                        Button
+                    </Button>
+                    <Button color="gray" size="large" fullWidth>
+                        Button
+                    </Button>
+                    <Button color="pink" size="large" fullWidth>
+                        Button
+                    </Button>
+                </ButtonGroup>
+            </AppBlock>
+        </ThemeProvider>
+    );
 }
 
 export default App;
